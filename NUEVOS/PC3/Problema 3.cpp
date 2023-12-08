f5896805ec14807bdd3637f171d57e0256747914
Gustavo Delgado <gustavo.delgado.r@uni.pe>
2023-12-08 12:37:46 -0500

    problema 3

--- NUEVOS/PC3/Problema 3.cpp
+++ NUEVOS/PC3/Problema 3.cpp
@@ -2,37 +2,91 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
-struct Generales
+//Declaracion de estructuras
+struct MiEstructura
 {
-	int B[2][2];
-	int suma;
+	int dato1[2][2];
+	int dato2;
 };
-int main() {
-    int f, i, g,j,k;
-    srand(time(NULL));
-    cout << "Ingrese cantidad de filas de la matriz: ";
-    cin >> f;
-    cout << "\n";
-    cout << "Ingrese cantidad de columnas de la matriz: ";
-    cin >> g;
-    cout << "\n";
-    float** A = new float* [f];
-    for (i = 0; i < f; i++){
-	    *(A + i) = new float[g];
-	}
-	int n=f*g;
-	Generales* particulares = new Generales[n];
-	    for (i=0;i<n;i++){
-    	int sumas=0;
-    	for (j=0;j<2;j++)
-    		for (k=0;k<2;k++)
-    			particulares[i].B[j][k]=1 + rand() % 5;
-    			sumas=sumas+particulares[i].B[j][k];
-     	particulares[i].suma=sumas;
-	}
-	
-	cout<<(particulares+1);
-	
-    
+//Declaracion de funciones
+void imprimirmatriz(MiEstructura***,int ,int );
+void imprimirmatriz(MiEstructura***,int ,int, int );
+int main(){
+	srand(time(NULL));
+	int filas, columnas;
+	//Tamaño de la matriz
+	cout<<"Numero de Filas de A: ";
+	cin>>filas;
+	cout<<"Numero de columnas de A: ";
+	cin>>columnas;
 
+	//Crea la matriz cuadrada de punteros a estructuras
+	MiEstructura*** matriz=new MiEstructura**[filas];
+	//define el maximo valor de la suma de los elementos internos
+	int maximo=0;
+	for (int i = 0; i < filas; i++)
+	{
+		*(matriz+i)=new MiEstructura*[columnas];
+		for (int j = 0; j < columnas; j++)
+		{
+			*(*(matriz + i) + j)=new MiEstructura();
+			int suma=0;
+			int aleatorio;
+			for (int p = 0; p < 2; p++)
+			{
+				for (int q = 0; q < 2; q++)
+				{
+					aleatorio=rand()%5+1;
+					matriz[i][j]->dato1[p][q]=aleatorio;
+					suma=suma+aleatorio;
+				}
+			}
+			matriz[i][j]->dato2=suma;
+			if (suma>maximo)
+			{
+				maximo=suma;
+			}
+			
+		}
+		
+	}
+	//imprimimos la matriz cuadrada
+	imprimirmatriz(matriz,filas,columnas);
+	//imprimimos los maximos
+	imprimirmatriz(matriz,filas,columnas,maximo);
+	
+	return 0;	
+}
+//definicion de funciones
+void imprimirmatriz(MiEstructura ***matriz,int filas,int columnas){
+	cout<<"Matriz A:"<<endl;
+	for (int i = 0; i < filas; i++)
+	{
+		for (int j = 0; j < columnas; j++)
+		{
+			cout<<"Matriz A["<<i<<"]["<<j<<"], (Suma="<<matriz[i][j]->dato2<<"). Direccion de memoria: "<<matriz[i][j]<<endl;
+			for (int p = 0; p < 2; p++)
+			{
+				for (int q = 0; q < 2; q++)
+				{
+					cout<<"B["<<p<<"]["<<q<<"] = "<<matriz[i][j]->dato1[p][q]<<" ";
+				}
+				cout<<endl;			
+			}
+		}
+	}
+}
+
+void imprimirmatriz(MiEstructura ***matriz,int filas,int columnas, int maximo){
+	cout<<"Matriz A:"<<endl;
+	for (int i = 0; i < filas; i++)
+	{
+		for (int j = 0; j < columnas; j++)
+		{
+			if(matriz[i][j]->dato2==maximo){
+			cout<<"Matriz A["<<i<<"]["<<j<<"], (Suma="<<matriz[i][j]->dato2<<"). Direccion de memoria: "<<matriz[i][j]<<endl;
+			}
+
+		}
+	}
 }
